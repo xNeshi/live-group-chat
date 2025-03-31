506eeff135b1fd9c294d2e0d1ecedf8dd5c66859
Liam Viray <neshiviray@gmail.com>
2025-04-01 00:38:43 +0800

    Enabled ppr

--- frontend/app/(root)/chats/page.tsx
+++ frontend/app/(root)/chats/page.tsx
@@ -1,39 +1,14 @@
-import ChatLogDialog from "@/components/ChatLogDialog";
-import { groupByDate } from "@/lib/utils";
+import ChatLogsArea from "@/components/ChatLogsArea";
 import React from "react";
 
-export const ChatsPage = async () => {
-  const res = await fetch("http://localhost:8080/api/chats");
-  const data = await res.json();
-  const groupedMessages = groupByDate(data);
+export const experimental_ppr = true;
 
+export const ChatsPage = async () => {
   return (
     <div className="flex flex-col items-center justify-center h-[calc(100vh-4rem)] px-4">
       <section className="flex flex-col gap-3 items-start max-w-[500px] h-[600px] ml:h-[80vh] w-full bg-card justify-start shadow-lg rounded-md py-5 px-4 ml:px-5 ml:py-6 -mt-20 ">
         <h1 className="text-[18px] pl-2">History</h1>
-        <div className="flex flex-col gap-3 w-full h-[500px] ml:h-[77vh] bg-card rounded-2xl overflow-auto">
-          {Object.keys(groupedMessages).map((dateData) => (
-            <div
-              key={dateData}
-              className="flex flex-col gap-3"
-            >
-              <div className="flex flex-row items-center gap-2 mb-2 opacity-30">
-                <hr className="flex-1 border-0 border-b-1" />
-                <h1 className="text-[12px]">{dateData}</h1>
-                <hr className="flex-1 border-0 border-b-1" />
-              </div>
-
-              {groupedMessages[dateData].map((messages, idx) => (
-                <div
-                  className="last:mb-4"
-                  key={idx}
-                >
-                  <ChatLogDialog message={messages} />
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
+        <ChatLogsArea />
       </section>
     </div>
   );
